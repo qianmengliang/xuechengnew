6f20080fe703286f3ac590c5f5168aaf1b638fcb
qianml <1160589162@qq.com>
2020-08-05 09:00:15 +0800

    提交

---
+++ xcEduCode01/xc-service-ucenter-auth/src/main/java/com/xuecheng/auth/controller/AuthController.java
@@ -0,0 +1,81 @@
+package com.xuecheng.auth.controller;
+
+import com.xuecheng.api.auth.AuthControllerApi;
+import com.xuecheng.auth.service.AuthService;
+import com.xuecheng.framework.domain.ucenter.ext.AuthToken;
+import com.xuecheng.framework.domain.ucenter.request.LoginRequest;
+import com.xuecheng.framework.domain.ucenter.response.AuthCode;
+import com.xuecheng.framework.domain.ucenter.response.LoginResult;
+import com.xuecheng.framework.exception.ExceptionCast;
+import com.xuecheng.framework.model.response.CommonCode;
+import com.xuecheng.framework.model.response.ResponseResult;
+import com.xuecheng.framework.utils.CookieUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @ClassName AuthController
+ * @Description: TODO
+ * @Author qianml
+ * @Date 2020/7/24
+ * @Version V1.0
+ **/
+@RestController
+@RequestMapping("/")
+public class AuthController implements AuthControllerApi {
+    @Value("${auth.clientId}")
+    String clientId;
+    @Value("${auth.clientSecret}")
+    String clientSecret;
+    @Value("${auth.cookieDomain}")
+    String cookieDomain;
+    @Value("${auth.cookieMaxAge}")
+    int cookieMaxAge;
+    @Value("${auth.tokenValiditySeconds}")
+    int tokenValiditySeconds;
+    @Autowired
+    AuthService authService;
+    @Override
+    @PostMapping("/userlogin")
+    public LoginResult login(LoginRequest loginRequest) {
+        //校验账号是否输入
+        if(loginRequest == null || StringUtils.isEmpty(loginRequest.getUsername())){
+            ExceptionCast.cast(AuthCode.AUTH_USERNAME_NONE);
+        }
+        //校验密码是否输入
+        if(StringUtils.isEmpty(loginRequest.getPassword())){
+            ExceptionCast.cast(AuthCode.AUTH_PASSWORD_NONE);
+        }
+        AuthToken authToken = authService.login(loginRequest.getUsername(),loginRequest.getPassword(), clientId, clientSecret);
+        //将令牌写入cookie
+        //访问token
+        String access_token = authToken.getAccess_token();
+        //将访问令牌存储到cookie
+        saveCookie(access_token);
+        return new LoginResult(CommonCode.SUCCESS,access_token);
+    }
+
+    /**
+     * /将令牌保存到cookie
+     * @param token
+     */
+    private void saveCookie(String token){
+        HttpServletResponse response = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getResponse();
+        //添加cookie 认证令牌，最后一个参数设置为false，表示允许浏览器获取
+        CookieUtil.addCookie(response, cookieDomain, "/", "uid", token, cookieMaxAge, false);
+    }
+
+    @Override
+    @PostMapping("/userlogout")
+    public ResponseResult logout() {
+        return null;
+    }
+}
